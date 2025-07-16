5149d7db9db7a9889ea7303ef7068c543cd15301
mohamedaminedridi <mohamedamine.dridi@esprit.tn>
2025-07-16 23:52:41 +0100

    /rememberme

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const upload = require('../middleware/upload'); // your multer setup
-const { registerUser, loginUser, getCurrentUser, approveBarbershop, approvePersonnel } = require('../controllers/authController');
 
-const { registerUser, loginUser } = require('../controllers/authController');
+const { registerUser, loginUser, getCurrentUser, approveBarbershop, approvePersonnel } = require('../controllers/authController');
 const { registerOwner } = require('../controllers/ownerController');
 const { registerPersonnel } = require('../controllers/personnelController');  // <--- Add this import
 
