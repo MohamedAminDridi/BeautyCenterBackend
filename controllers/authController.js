babf07f2eb6a7739125f6b364d6ef94eee07564c
mohamedaminedridi <mohamedamine.dridi@esprit.tn>
2025-07-18 20:18:38 +0100

    /toke changed to _id:newUser

--- controllers/authController.js
+++ controllers/authController.js
@@ -140,7 +140,7 @@ const loginUser = async (req, res) => {
     }
 
     const token = jwt.sign(
-      { id: user._id, role: user.role },
+      { _id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: '1d' }
     );
